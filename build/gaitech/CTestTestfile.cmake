677fd0c3cbc17ccdeb5d2995e850974e5045440c
RamrajSegur <ramraj.dinesh@gmail.com>
2017-02-02 13:39:48 -0500

    initial commit1

---
+++ build/gaitech/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ramraj/catkin_ws/src/gaitech
+# Build directory: /home/ramraj/catkin_ws/build/gaitech
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
